99559bd100d165c824e3899e1182c542c987eb81
d.zarzycki <dz@gm.com>
2021-06-18 15:59:04 +0200

    zwiększenie pkt życia kapitana bomby

--- 2. Java - podstawy programowanie/6. Warrior/Main.java
+++ 2. Java - podstawy programowanie/6. Warrior/Main.java
@@ -3,7 +3,7 @@ package pl.sdacademy.warrior;
 public class Main {
     public static void main(String[] args) {
         Warrior heMan = new Warrior("He-man", 20, 150);
-        Warrior kapitanBomba = new Warrior("Kapitan Bomba", 15, 100);
+        Warrior kapitanBomba = new Warrior("Kapitan Bomba", 15, 110);
         System.out.println(heMan);
         System.out.println(kapitanBomba);
 
